22766ac49be77945209117e68e4e8d2be17661c6
Retii <109904771+RetiiAyo@users.noreply.github.com>
2023-04-28 17:49:09 +0200

    Create loader.lua

---
+++ ProjectUwu/loader.lua
@@ -0,0 +1,37 @@
+--[[
+
+ ,ggggggggggg,                                                  ,ggg,         gg                             
+dP"""88""""""Y8,                                           I8  dP""Y8a        88                             
+Yb,  88      `8b                                           I8  Yb, `88        88                             
+ `"  88      ,8P                   gg                   88888888`"  88        88                             
+     88aaaad8P"                    ""                      I8       88        88                             
+     88""""",gggggg,    ,ggggg,    gg   ,ggg,     ,gggg,   I8       88        88  gg    gg    gg  gg      gg 
+     88     dP""""8I   dP"  "Y8ggg 8I  i8" "8i   dP"  "Yb  I8       88        88  I8    I8    88bgI8      8I 
+     88    ,8'    8I  i8'    ,8I  ,8I  I8, ,8I  i8'       ,I8,      88        88  I8    I8    8I  I8,    ,8I 
+     88   ,dP     Y8,,d8,   ,d8'_,d8I  `YbadP' ,d8,_    _,d88b,     Y8b,____,d88,,d8,  ,d8,  ,8I ,d8b,  ,d8b,
+     88   8P      `Y8P"Y8888P"888P"888888P"Y888P""Y8888PP8P""Y8      "Y888888P"Y8P""Y88P""Y88P"  8P'"Y88P"`Y8
+                                 ,d8I'                                                                       
+                               ,dP'8I                                                                        
+                              ,8"  8I                                                                        
+                              I8   8I                                                                        
+                              `8, ,8I                                                                        
+                               `Y8P"                                                                         
+
+ProjectUwu - Loader # Retii#4208
+
+]]
+
+-- [[ Checking ]]
+
+if not syn then
+  return warn("[ProjectUwu]: Your executor does not have 'syn' module. Get help or install a better one.")
+end
+
+--[[ Teleport ]]
+
+sy.queue_on_teleport([[
+  loadstring(game:HttpGet(""))()
+]])
+
+--[[ Script ]]
+
