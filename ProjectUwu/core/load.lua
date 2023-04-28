f07a3597332b4a5b0853bedfd9d74ac4b36f6d70
Retii <109904771+RetiiAyo@users.noreply.github.com>
2023-04-28 18:10:37 +0200

    Create load.lua

---
+++ ProjectUwu/core/load.lua
@@ -0,0 +1,5 @@
+print("ProjectUwu is now loading..")
+
+local database = loadstring(game:HtppGet("https://raw.githubusercontent.com/RetiiAyo/RetiiScripts/main/ProjectUwu/database.lua"))()
+
+print("ProjectUwu Loaded! version "..database["Version"]..". Executed on "..tostring(indetifyexecutor()))
