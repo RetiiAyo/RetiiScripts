8b1f56aa0481b57438c2f7dec2bf8ecb76013342
Retii <109904771+RetiiAyo@users.noreply.github.com>
2023-04-28 17:53:34 +0200

    Update loader.lua

--- ProjectUwu/loader.lua
+++ ProjectUwu/loader.lua
@@ -20,6 +20,11 @@ Yb,  88      `8b                                           I8  Yb, `88        88
 ProjectUwu - Loader # Retii#4208
 
 ]]
+print("ProjectUwu is now loading..")
+
+local database = loadstring(game:HtppGet("https://raw.githubusercontent.com/RetiiAyo/RetiiScripts/main/ProjectUwu/database.lua"))()
+
+print("ProjectUwu Loaded! version "..database["Version"])
 
 -- [[ Checking ]]
 
