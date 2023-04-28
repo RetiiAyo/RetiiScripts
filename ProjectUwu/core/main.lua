6596564e7cc3bfdcb91a83765143b6b05e031ac9
Retii <109904771+RetiiAyo@users.noreply.github.com>
2023-04-28 18:26:54 +0200

    Update main.lua

--- ProjectUwu/core/main.lua
+++ ProjectUwu/core/main.lua
@@ -1,2 +1,12 @@
 local id = game.PlaceId
+local url = "https://raw.githubusercontent.com/RetiiAyo/RetiiScripts/main/ProjectUwu/games/"..id..".lua"
 
+local success, err = pcall(function()
+    loadstring(game:HttpGet(url))()
+end)
+
+if success then
+  print("loading - success")
+  else
+  print(err)
+end
